3e2a0933ee19959c8d5a9508f5118be979274a8a
d62remi <d62remi@gmail.com>
2017-04-26 17:31:45 +0200

    Revert "Revert "Add watch option to build script""
    This reverts commit ff87f3a5f8d53c78b85e554915437f7f7ed9fb11.

--- test/src/App.js
+++ test/src/App.js
@@ -6,7 +6,6 @@ import Refresher from './../../index.js';
 class App extends Component {
   render() {
     return (
-
       <div className="App">
         <Refresher/>
         <div className="App-header">
@@ -15,7 +14,7 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>       
       </div>
     );
   }
